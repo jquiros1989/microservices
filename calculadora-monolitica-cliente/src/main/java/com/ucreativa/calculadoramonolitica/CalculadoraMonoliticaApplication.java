0b87c8ed603f4a1a333b76bdea9ea7171530fe32
Josué Quirós Abarc <jquiros1989@gmail.com>
2021-07-20 22:56:51 -0600

    Adding source code for the client

---
+++ calculadora-monolitica-cliente/src/main/java/com/ucreativa/calculadoramonolitica/CalculadoraMonoliticaApplication.java
@@ -0,0 +1,13 @@
+package com.ucreativa.calculadoramonolitica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraMonoliticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraMonoliticaApplication.class, args);
+	}
+
+}
