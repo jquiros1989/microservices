d0086aee27ec18d089d7c5816a825c841a158579
Josué Quirós Abarc <jquiros1989@gmail.com>
2021-07-20 21:57:11 -0600

    Commit to test where this goes

--- calculadora-monolitica/src/main/java/com/ucreativa/calculadoramonolitica/CalculadoraMonoliticaApplication.java
+++ calculadora-monolitica/src/main/java/com/ucreativa/calculadoramonolitica/CalculadoraMonoliticaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CalculadoraMonoliticaApplication {
 
 	public static void main(String[] args) {
+		/** Comment **/
 		SpringApplication.run(CalculadoraMonoliticaApplication.class, args);
 	}
 
